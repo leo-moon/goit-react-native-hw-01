ecd58c9ba21fed0e148337d30d8bdf476ee75e4b
leo-moon <49075905+leo-moon@users.noreply.github.com>
2023-03-14 00:35:03 +0200

    fin 01

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <Text>Crazy world</Text> */}
+      <Text>Crazy world</Text>
       <Text>Viva GoIT!!!</Text>
       <StatusBar style="auto" />
     </View>
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#01dd45',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
