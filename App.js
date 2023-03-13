288c6880f07848f2e883a7925ec89e7c73343435
leo-moon <49075905+leo-moon@users.noreply.github.com>
2023-03-14 00:25:18 +0200

    start

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* <Text>Crazy world</Text> */}
+      <Text>Viva GoIT!!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#01dd45',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
